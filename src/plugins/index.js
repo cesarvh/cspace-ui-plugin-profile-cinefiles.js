b40bd7585dc2f9e51512956f53de69a7b493016e
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-03-07 16:49:19 -0800

    BAMPFA-583: Hid collectionobject fields that are not needed

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,18 @@
+<<<<<<< HEAD
 import recordTypes from './recordTypes';
 import optionLists from './optionLists';
 
 export default [
   ...recordTypes,
   ...optionLists,
+=======
+// import extensions from './extensions';
+// import optionLists from './optionLists';
+import recordTypes from './recordTypes';
+
+export default [
+//   ...extensions,
+//   ...optionLists,
+  ...recordTypes,
+>>>>>>> BAMPFA-583: Hid collectionobject fields that are not needed
 ];
