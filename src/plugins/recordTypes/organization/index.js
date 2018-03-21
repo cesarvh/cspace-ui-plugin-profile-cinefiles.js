645278091a8a9190c74e94e5e1a2742a985de599
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-03-20 21:12:40 -0700

    BAMPFA-589: Customized organization advanced search page

--- src/plugins/recordTypes/organization/index.js
+++ src/plugins/recordTypes/organization/index.js
@@ -1,11 +1,13 @@
 import forms from './forms';
 import vocabularies from './vocabularies';
 import fields from './fields';
+import advancedSearch from './advancedSearch';
 
 export default () => pluginContext => ({
   recordTypes: {
     organization: {
       vocabularies,
+      advancedSearch: advancedSearch(pluginContext),
       forms: forms(pluginContext),
       fields: fields(pluginContext),
     },
